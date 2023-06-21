71e70ee51acd12b466fee56a4231fedc6f43d700
Bruno Serpa <brunospenna@gmail.com>
2023-06-20 21:30:30 -0300

    req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,31 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
+from src.models.ingredient import Ingredient, Restriction
 
 # Req 1
+
 def test_ingredient():
-    pass
+    bacon = Ingredient("bacon")
+    bacon_2 = Ingredient("bacon")
+    assert bacon == bacon_2
+
+    bacon = Ingredient("bacon")
+    bacon_2 = Ingredient("bacon")
+    assert hash(bacon) == hash(bacon_2)
+
+    bacon = Ingredient("bacon")
+    queijo = Ingredient("queijo")
+    assert not bacon == queijo
+
+    bacon = Ingredient("bacon")
+    queijo = Ingredient("queijo")
+    assert hash(bacon) != hash(queijo)
+
+    bacon = Ingredient("bacon")
+    assert bacon.name == "bacon"
+
+    bacon = Ingredient("bacon")
+    assert repr(bacon) == "Ingredient('bacon')"
+
+    bacon = Ingredient("bacon")
+    bacon_restriction = {Restriction.ANIMAL_DERIVED, Restriction.ANIMAL_MEAT}
+    assert bacon.restrictions == bacon_restriction
